ff8da1cc6a239b26b419f45c74a2819639031ca2
kobakobagit <kigmikfe14@gmail.com>
2021-10-31 15:21:13 +0900

    init commit

---
+++ src/main/java/keigo/clothesApp/ClothesAppApplication.java
@@ -0,0 +1,13 @@
+package keigo.clothesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/keigo/clothesApp/ClothesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package keigo.clothesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

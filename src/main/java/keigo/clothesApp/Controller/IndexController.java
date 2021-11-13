ec820b14c9314ff0348d6739d7e6c4bb19797536
kobakobagit <kigmikfe14@gmail.com>
2021-11-13 21:40:04 +0900

    DB接続確認

--- src/main/java/keigo/clothesApp/Controller/IndexController.java
+++ src/main/java/keigo/clothesApp/Controller/IndexController.java
@@ -1,13 +1,27 @@
 package keigo.clothesApp.Controller;
 
+import java.util.List;
+import keigo.clothesApp.domain.Size;
+import keigo.clothesApp.service.SizeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
+@RequestMapping("/")
 public class IndexController {
 
+    private final SizeService sizeService;
+
+    public IndexController(SizeService sizeService) {
+        this.sizeService = sizeService;
+    }
+
     @GetMapping("/")
-    public String index() {
-        return "Hello World";
+    public String size(Model model) {
+        List<Size> sizeList = sizeService.getAllSize();
+        model.addAttribute("sizeList", sizeList);
+        return "/size";
     }
 }
